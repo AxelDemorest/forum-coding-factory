a13f160914cbcd00f30a339c1a5baf6d4ba71531
Axel Demorest <axe.demorest@gmail.com>
2020-12-09 01:04:41 +0100

    passage bootstrap 5

--- public/header/header.php
+++ public/header/header.php
@@ -3,82 +3,69 @@
 if (isset($_SESSION['auth'])) : ?>
 
     <!-- Navbar de l'utilisateur connecté -->
-    <div class="w-100 d-flex align-items-center" style="height: 70px">
-        <nav class="navbar navbar-expand-lg fixed-top navbar-light bg-white fz-text" style="height: 70px">
-            <div>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <a class="navbar-brand" href="/forum-coding-factory/public/home/home.php">
-                    <img class="ml-4" src="../../img/logocodingfactory-ptt.jpg" width="190" alt="" loading="lazy">
-                </a>
-            </div>
-            <div class="d-none d-md-inline-block d-lg-none">
-                <a href="/forum-coding-factory/public/account/account.php"><button type="button" class="btn btn-outline-secondary mr-md-4">Profil</button></a>
-                <a href="/forum-coding-factory/public/deconnexion/deconnexion.php"><button type="button" class="btn btn-danger mr-md-4">Se déconnecter</button></a>
-            </div>
-            <div class="collapse navbar-collapse" id="navbarText">
-                <ul class="navbar-nav mr-auto pl-lg-3">
-                    <li class="nav-item active">
-                        <a class="nav-link px-lg-4 pt-4 pt-lg-2" href="/forum-coding-factory/public/home/home.php">Accueil</a>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top py-2 fz-text">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="/forum-coding-factory/public/home/home.php">
+                <img class="ms-4" src="../../img/logocodingfactory-ptt.jpg" width="190" alt="" loading="lazy">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item px-lg-3 pt-2 pt-lg-0">
+                        <a class="nav-link active text-danger" aria-current="page" href="/forum-coding-factory/public/home/home.php">Accueil</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link px-lg-4" href="#">Équipe</a>
+                    <li class="nav-item px-lg-3">
+                        <a class="nav-link" href="#">Équipe</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link px-lg-4" href="#">Forum</a>
+                    <li class="nav-item px-lg-3">
+                        <a class="nav-link" href="#">Forum</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link px-lg-4" href="#">Nouveautés</a>
+                    <li class="nav-item px-lg-3">
+                        <a class="nav-link" href="#">Nouveautés</a>
                     </li>
                 </ul>
-                <div class="d-flex justify-content-center d-md-none d-lg-inline-block">
-                    <a href="/forum-coding-factory/public/account/account.php"><button type="button" class="btn btn-outline-secondary mr-4">Profil</button></a>
-                    <a href="/forum-coding-factory/public/deconnexion/deconnexion.php"><button type="button" class="btn btn-danger mr-md-4">Se déconnecter</button></a>
+                <div class="d-flex justify-content-center">
+                    <a href="/forum-coding-factory/public/account/account.php"><button type="button" class="btn btn-outline-secondary me-4">Profil</button></a>
+                    <a href="/forum-coding-factory/public/deconnexion/deconnexion.php"><button type="button" class="btn btn-danger me-md-4">Se déconnecter</button></a>
                 </div>
             </div>
-        </nav>
-    </div>
+        </div>
+    </nav>
 
 <?php else : ?>
 
     <!-- Navbar de l'utilisateur non connecté -->
-    <div class="w-100 d-flex align-items-center" style="height: 70px">
-        <nav class="navbar navbar-expand-lg fixed-top navbar-light bg-white fz-text" style="height: 70px">
-            <div>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <a class="navbar-brand" href="/forum-coding-factory/public/home/home.php">
-                    <img class="ml-4" src="../../img/logocodingfactory-ptt.jpg" width="190" alt="" loading="lazy">
-                </a>
-            </div>
-            <div class="d-none d-md-inline-block d-lg-none">
-                <a href="/forum-coding-factory/public/inscription-connexion/connexion.php"><button type="button" class="btn btn-outline-secondary mr-md-4">Se connecter</button></a>
-                <a href="/forum-coding-factory/public/inscription-connexion/inscription.php"><button type="button" class="btn btn-danger mr-md-4">S'inscrire</button></a>
-            </div>
-            <div class="collapse navbar-collapse" id="navbarText">
-                <ul class="navbar-nav mr-auto pl-lg-3">
-                    <li class="nav-item active">
-                        <a class="nav-link px-lg-4 pt-4 pt-lg-2" href="/forum-coding-factory/public/home/home.php">Accueil</a>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top py-2 fz-text">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="/forum-coding-factory/public/home/home.php">
+                <img class="ms-4" src="../../img/logocodingfactory-ptt.jpg" width="190" alt="" loading="lazy">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item px-lg-3 pt-2 pt-lg-0">
+                        <a class="nav-link active text-danger" aria-current="page" href="/forum-coding-factory/public/home/home.php">Accueil</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link px-lg-4" href="#">Équipe</a>
+                    <li class="nav-item px-lg-3">
+                        <a class="nav-link" href="#">Équipe</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link px-lg-4" href="#">Forum</a>
+                    <li class="nav-item px-lg-3">
+                        <a class="nav-link" href="#">Forum</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link px-lg-4" href="#">Nouveautés</a>
+                    <li class="nav-item px-lg-3">
+                        <a class="nav-link" href="#">Nouveautés</a>
                     </li>
                 </ul>
-                <div class="d-flex justify-content-center d-md-none d-lg-inline-block">
-                    <a href="/forum-coding-factory/public/inscription-connexion/connexion.php"><button type="button" class="btn btn-outline-secondary mr-4">Se connecter</button></a>
-                    <a href="/forum-coding-factory/public/inscription-connexion/inscription.php"><button type="button" class="btn btn-danger mr-md-4">S'inscrire</button></a>
+                <div class="d-flex justify-content-center">
+                    <a href="/forum-coding-factory/public/inscription-connexion/connexion.php"><button type="button" class="btn btn-outline-secondary me-4">Se connecter</button></a>
+                    <a href="/forum-coding-factory/public/inscription-connexion/inscription.php"><button type="button" class="btn btn-danger me-md-4">S'inscrire</button></a>
                 </div>
             </div>
-        </nav>
-    </div>
-
+        </div>
+    </nav>
 
 <?php endif; ?>
--- public/home/home.php
+++ public/home/home.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
     <link href="../header/header.css" rel="stylesheet" />
     <link href="../footer/footer.css" rel="stylesheet" />
@@ -26,8 +26,8 @@
                     <div class="bg-dark p-3 opacity-bg">
                         <h1 class="text-white">Learn, Together</h1>
                     </div>
-                    <form class="form-inline my-5 w-100 d-flex justify-content-center fz-text">
-                        <input class="form-control mr-sm-2 w-75" type="search" placeholder="Recherche un sujet" aria-label="Search">
+                    <form class="form-inline my-5 w-100 d-none d-md-flex justify-content-md-center fz-text">
+                        <input class="form-control me-sm-2 w-75" type="search" placeholder="Recherche un sujet" aria-label="Search">
                         <button class="btn btn-danger my-2 my-sm-0" type="submit">Search</button>
                     </form>
                 </div>
@@ -46,7 +46,7 @@
                     Différentes fonctionnalités sont dispnibles pour que l'expérience utilisateur soit la meilleure
                     possible. Les élèves pourront avancer ensemble et progresser rapidement avec l'entraide.</p>
             </div>
-            <div class="col-12 col-lg-6 pl-lg-5 pt-5 pt-lg-0 d-flex justify-content-center">
+            <div class="col-12 col-lg-6 ps-lg-5 pt-5 pt-lg-0 d-flex justify-content-center">
                 <img class="img-fluid" src="../../img/web-design.gif">
             </div>
         </div>
@@ -64,26 +64,13 @@
 
     ?>
 
-    <!-- <?php foreach ($resultat as $listUser => $userParameter) : ?>
-
-                <div class="col-2">
-                <?php
-                echo $userParameter['mail'];
-                echo " - ";
-                echo $userParameter['dateInscription'];
-                echo "<br />";
-                ?>
-
-                <?php endforeach; ?>
-                </div> -->
-
     <!-- Container de la liste des nouveaux utilisateurs -->
     <div class="bg-light">
         <div class="container-fluid">
             <h1 class="text-center py-4">Les derniers utilisateurs inscrits !</h1>
             <div class="row justify-content-center">
             <?php foreach ($resultat as $listUser => $userParameter) : ?>
-                <div class="col-2 mx-4 mb-4 mt-2 border border-secondary rounded fz-text" style="height: 10em;">
+                <div class="col-8 col-sm-4 col-lg-2 mx-4 mb-4 mt-2 border border-secondary rounded fz-text" style="height: 10em;">
                     <h5 class="text-center pt-3"><?php echo $userParameter['pseudo']; ?></h5>
                     <div class="bg-danger mx-auto rounded-pill" style="height: 4px; width: 20%;"></div>
                     <p class="text-center pt-2 mb-1">Date de création :</p>
@@ -97,9 +84,7 @@
     <?php include "../footer/footer.html"; ?>
 
     <!-- Javascript -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
     <script src="home.js"></script>
 </body>
 
