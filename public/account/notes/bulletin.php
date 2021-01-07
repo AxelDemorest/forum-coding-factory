f70e6f22bc9e66e2240d2a3b32ce2bf1d3182acf
Axel Demorest <axe.demorest@gmail.com>
2021-01-07 15:23:05 +0100

    correction bulletin.php

--- public/account/notes/bulletin.php
+++ public/account/notes/bulletin.php
@@ -17,15 +17,15 @@
 
     <?php session_start();
 
-    include "../header/header.php"; ?>
+    include "../../header/header.php"; ?>
 
     <!-- Tu peux coder ici -->
 
     <!-- Footer -->
-    <?php include "../footer/footer.html"; ?>
+    <?php include "../../footer/footer.html"; ?>
 
     <!-- Javascript -->
-    <script src="../header/header.js"></script>
+    <script src="../../header/header.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 </body>
 
